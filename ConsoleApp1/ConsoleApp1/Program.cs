bdbd4234f5b502eb5f3bd6c6ed9a0dd5ab929545
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-07 19:31:25 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 28);
+            Console.WriteLine(number);
+
+            for (int i = number; i < 1000 & i > 100 ; i += number)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
